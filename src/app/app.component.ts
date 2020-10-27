ad90e980575fa095f9f099d9fb076552e963510e
anthony mero <anthony@gladys.io>
2020-10-27 16:59:37 +0100

    - Applying theme on components with overlay

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { OverlayContainer } from '@angular/cdk/overlay';
 import { Component, HostBinding, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import { ThemingService } from './theming.service';
@@ -8,25 +9,44 @@ import { ThemingService } from './theming.service';
   styleUrls: ['./app.component.scss']
 })
 
-export class AppComponent implements OnInit, OnDestroy{
+export class AppComponent implements OnInit, OnDestroy {
   themingSubscription: Subscription;
 
   themeColor = 'primary';
 
   constructor(
     private readonly themingService: ThemingService,
-  ) {}
+    private overlayContainer: OverlayContainer,
+
+  ) { }
 
   @HostBinding('class') public cssClass: string;
 
   ngOnInit(): void {
     this.themingSubscription =
-    this.themingService.theme.subscribe((theme: string) => {
-      this.cssClass = theme;
-    });
+      this.themingService.theme.subscribe((theme: string) => {
+        this.cssClass = theme;
+        this.applyThemeOnOverlays();
+      });
   }
 
   ngOnDestroy(): void {
     this.themingSubscription.unsubscribe();
+  }
+
+
+// Private method
+/*
+* Apply current theme on components with overlay (e.g. dropdowns, dialogs)
+*/
+  private applyThemeOnOverlays(): void {
+    // remove old theme class and add new theme class
+    // we're removing any css class that contains '-theme' string but your theme classes can follow any pattern
+    const overlayContainerClasses = this.overlayContainer.getContainerElement().classList;
+    const themeClassesToRemove = Array.from(this.themingService.themes);
+    if (themeClassesToRemove.length) {
+      overlayContainerClasses.remove(...themeClassesToRemove);
+    }
+    overlayContainerClasses.add(this.cssClass);
   }
 }
